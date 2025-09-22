1b459ca03cbc33542796c8dc2614294b9a8c653c
Amitlaxman <AMITLAXMAN.M@gmail.com>
2025-09-22 10:11:02 +0000

    Try fixing this error: `Runtime Error: Error: FormDescription is not def

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -10,7 +10,7 @@ import * as z from 'zod';
 
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, FormDescription } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { useToast } from '@/hooks/use-toast';
